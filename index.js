49abdcd282bd8595b0fce41ac94ef13ef1830afb
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 14:43:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
+var name = "zhifeng"
 var height = "74"
 
 console.log("Name:")
-console.log("zhifeng ou")
+console.log(name)
 
 console.log("Height:")
-console.log("170cm")
+console.log(height)
